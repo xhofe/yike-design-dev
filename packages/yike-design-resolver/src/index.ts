1939f862980bab6774a880c15b2dff2421133ac9
rickon-DAFEI <1016831404@qq.com>
2023-10-04 23:23:36 +0800

    feat(resolver): dev resolver

--- packages/yike-design-resolver/src/index.ts
+++ packages/yike-design-resolver/src/index.ts
@@ -1,15 +1,21 @@
-import { parseImportStatements, fetchIndexContent } from './utils';
-const yikeSrcPath = '@yike-design/ui/es';
+import { sideEffects, parseImportStatements, fetchIndexContent } from './utils';
 const compPaths: any = {};
 const content = fetchIndexContent();
 parseImportStatements(compPaths, content);
+const isDev = process.env.NODE_ENV === 'development';
 export function YikeResolver(compName: string) {
+  let yikeSrcPath = '@yike-design/ui/es';
+  const resolverMap = {
+    name: compName,
+    from: yikeSrcPath,
+    sideEffects: sideEffects(compPaths[compName]),
+  };
+  if (isDev) {
+    yikeSrcPath = '@yike-design/ui/src';
+    resolverMap.sideEffects = '';
+  }
   if (compName.startsWith('Yk')) {
-    return {
-      name: compName,
-      from: yikeSrcPath,
-      // sideEffects: sideEffects(compPaths[compName]),
-    };
+    return resolverMap;
   }
   if (compName.startsWith('Icon')) {
     return {
